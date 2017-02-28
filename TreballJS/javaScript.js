1118f0c6f76fb407327acdc338c3cbf617a6015c
jlloret <jlloret@INF2-PC15>
2017-02-28 19:27:51 +0100

    test insti

--- TreballJS/javaScript.js
+++ TreballJS/javaScript.js
@@ -57,13 +57,4 @@ function insertarDatosTabla(posicion){
     
 }
 
-/*<tr>
- <td>0</td>
- <td>0</td>
- <td>1</td>
- <td>2</td>
- <td>3</td>
- <td>4</td>
- <td>5</td>
- </tr>*/
-                
+      
