74fd6ecdb23209054fcd84829d7bd22d268725d2
Jesus <Jesus@192.168.1.114>
2017-02-28 12:25:42 +0100

    ARCHIVOS, ajax

--- TreballJS/javaScript.js
+++ TreballJS/javaScript.js
@@ -1,7 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+var txtDatos;
+//La posicion [0]=id, [1]=Descripcion [2]=unidades/caja, [3]=precio*caja [4]=cajas [5]=total€
+var contenido = [];
+function cargaArchivo() {
+    $.ajax({
+        url: "./documentos/Jardin.csv",
+        success: function (data) {
+            //Estamos guardandolo en un variable gloval para poder acceder a los datos desde fuera
+            txtDatos = data;
+            //Lo que va hacer es recortar los datos y organizarlos en el array
+            leerDatos();
+        }
+    });
+}
+//Llamada a la funcion para cargar los datos
+cargaArchivo();
 
+function leerDatos(){
+    
+}
