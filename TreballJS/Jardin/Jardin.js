fce1ef3f8d1d1b8ea383a5e5bf48aa3f79fae58d
jlloret <jlloret@INF2-PC15>
2017-03-03 19:26:12 +0100

    Imagen grande

--- TreballJS/Jardin/Jardin.js
+++ TreballJS/Jardin/Jardin.js
@@ -26,7 +26,7 @@ function leerDatos() {
         arrayHijo.push(lineas.split(";")[0]);
         arrayHijo.push(lineas.split(";")[1]);
         arrayHijo.push(lineas.split(";")[2]);
-        arrayHijo.push(lineas.split(";")[3].replace(",","."));
+        arrayHijo.push(lineas.split(";")[3].replace(",", "."));
         arrayHijo.push(0);
         arrayHijo.push(0);
         //Metemos a la array hijo dentro de la padre
@@ -35,46 +35,56 @@ function leerDatos() {
     }
 }
 //Esta es la funcion necesaria para poder insertar los datos en cada fila de la tabla, se creara dinamicamente
-function insertarDatosTabla(posicion){
+function insertarDatosTabla(posicion) {
     var fila;
-    if (posicion%2==0){
-        fila="<tr class='success'>";
+    if (posicion % 2 === 0) {
+        fila = "<tr class='success'>";
+    } else {
+        fila = "<tr class='info'>";
     }
-    else{
-        fila="<tr class='info'>";
-    } 
-    
-   var foto = arrayPadre[posicion][0];
-    fila+= "<td><img src='http://www.vetusplas.com/images/webcli/peque/"+foto+".png' onclick=()'></td>";
-    fila+= "<td>"+arrayPadre[posicion][0]+"</td>";
-    fila+= "<td>"+arrayPadre[posicion][1]+"</td>";
-    fila+= "<td>"+arrayPadre[posicion][2]+"</td>";
-    fila+= "<td>"+arrayPadre[posicion][3]+"</td>";
-    fila+= "<td><input type='number' id='cajas"+posicion+"' min='0' oninput='calculoTotal("+posicion+")'></td>";
-    fila+= "<td><input type='number' disabled id='total"+posicion+"'></td>";
-    fila+= "</tr>";
+
+    var foto = arrayPadre[posicion][0];
+    fila += "<td><img src='http://www.vetusplas.com/images/webcli/peque/" + foto + ".png' onclick='imagengrande("+posicion+")' id='foto"+posicion+"'></td>";
+    fila += "<td>" + arrayPadre[posicion][0] + "</td>";
+    fila += "<td>" + arrayPadre[posicion][1] + "</td>";
+    fila += "<td>" + arrayPadre[posicion][2] + "</td>";
+    fila += "<td>" + arrayPadre[posicion][3] + "</td>";
+    fila += "<td><input type='number' id='cajas" + posicion + "' min='0' oninput='calculoTotal(" + posicion + ")'></td>";
+    fila += "<td><input type='number' disabled id='total" + posicion + "'></td>";
+    fila += "</tr>";
     //Cada vez que hago un fila no borre la anterior
-    document.getElementById("tbody").innerHTML+=fila;
-    
+    document.getElementById("tbody").innerHTML += fila;
+
 }
 
-function calculoTotal(dato){
-    var numCaja=arrayPadre[dato][4]=parseInt(document.getElementById("cajas"+dato).value);
+function calculoTotal(dato) {
+    var numCaja = arrayPadre[dato][4] = parseInt(document.getElementById("cajas" + dato).value);
     console.log(arrayPadre[dato]);
     console.log(dato);
-    
-    var udCaja=parseInt(arrayPadre[dato][2]);
-    var precio=parseFloat(arrayPadre[dato][3]);
-    
-    var total=(udCaja*precio)*numCaja;
+
+    var udCaja = parseInt(arrayPadre[dato][2]);
+    var precio = parseFloat(arrayPadre[dato][3]);
+
+    var total = (udCaja * precio) * numCaja;
     console.log(total);
-    
-    arrayPadre[dato][5]=total;
+
+    arrayPadre[dato][5] = total;
     console.log(arrayPadre[dato]);
-    
-    document.getElementById("total"+dato).value=Math.round(total*100)/100;
+
+    document.getElementById("total" + dato).value = Math.round(total * 100) / 100;
 }
-      
-function imagengrande(){
-     window.open();
+
+function imagengrande(dato) {
+    var altura = 1312;
+    var anchura = 510;
+    var foto = arrayPadre[dato][0];
+    console.log(foto);
+// calculamos la posicion x e y para centrar la ventana
+    var y = parseInt((window.screen.height / 2) - (altura / 2));
+    var x = parseInt((window.screen.width / 2) - (anchura / 2));
+
+// mostramos la ventana centrada
+ 
+   window.open('http://www.vetusplas.com/images/webcli/grande/'+foto+'.png',target='blank','width='+anchura+',height='+altura+',top='+y+',left='+x+',toolbar=no,location=no,status=no,menubar=no,scrollbars=no,directories=no,resizable=no',title='testing');
+ 
 }
